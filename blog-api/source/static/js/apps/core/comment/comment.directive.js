8b0a16f9fd7b6f0482f5209b65b9b61ea64271f0
Michel <michel.rodrigues86@yahoo.com.br>
2016-12-29 17:44:01 -0200

    022 Comment Reply Directive Part 2

--- blog-api/source/static/js/apps/core/comment/comment.directive.js
+++ blog-api/source/static/js/apps/core/comment/comment.directive.js
@@ -5,6 +5,7 @@ commentModule.directive('commentReplyThread', function(Comment){
     restrict: "E",
     scope: {
       comment: "=comment",
+      slug: "=slug",
     },
     template: `
       <ul><li ng-repeat="reply in replies">{{reply.content}}</li></ul>
@@ -12,7 +13,11 @@ commentModule.directive('commentReplyThread', function(Comment){
         <i class="fa fa-spinner fa-pulse fa-2x fa-fw"></i>
         <span class="sr-only">Loading...</span>
       </div>
-      `,
+      <p style="color: red;" ng-if="reply.content" >PREVIEW: {{reply.content}}</p>
+      <form ng-submit="addCommentReply(reply, comment)">
+        <textarea ng-model="reply.content"></textarea>
+        <input class="btn btn-default" type="submit">
+      </form>`,
     link: function(scope, element, attr){
       var commentId = scope.comment.id
       if (scope.comment){
@@ -25,8 +30,29 @@ commentModule.directive('commentReplyThread', function(Comment){
           );
         }
       }
+      scope.addCommentReply = function(reply, parentComment){
+        Comment.create(
+            {
+            content: reply.content,
+            slug: scope.slug,
+            type: 'post',
+            parent_id: parentComment.id
+          },
+          function(data){
+            //success
+            console.log(data);
+            scope.comments.replies.push(data);
+            parentComment.reply_count += 1;
+            reply.content = "";
+            //resetReply();
+          },
+          function(e_data){
+            //error
+            console.log(e_data);
+          }
+        )
+      }
+
     }
   };
 });
-
-
